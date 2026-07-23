c3771e78ce5b7bc9474367336ceea11b1d06b4ad
samgator <sam.morsics@gmail.com>
2026-07-23 14:09:56 -0400

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
         <main className="flex-1 w-full flex flex-col">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
